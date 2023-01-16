787adc1c5095fef0665b7d1c1c4c75c01f9a72a1
AndreGuima <andrepaivaguimaraes@gmail.com>
2023-01-16 14:11:45 -0300

    terminando o login do usuario e Dockerizando a aplicacao

--- src/main/java/br/com/gerenciador/controller/action/RemoveConta.java
+++ src/main/java/br/com/gerenciador/controller/action/RemoveConta.java
@@ -5,19 +5,25 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.gerenciador.controller.action.interfaces.Acao;
 import br.com.gerenciador.model.BancoDeDados;
+import br.com.gerenciador.model.Usuario;
 
 public class RemoveConta implements Acao {
 
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String paramId = request.getParameter("id");
-		Integer id = Integer.valueOf(paramId);
+		HttpSession sessao = request.getSession();
+		Usuario usuarioLogado = (Usuario) sessao.getAttribute("usuarioLogado");
+		if (usuarioLogado != null) {
+			String paramId = request.getParameter("id");
+			Integer id = Integer.valueOf(paramId);
 
-		BancoDeDados banco = new BancoDeDados();
-		banco.removeById(id);
+			BancoDeDados banco = new BancoDeDados();
+			banco.removeContaById(usuarioLogado, id);
+		}
 
 		return "redirect:entrada?acao=ListaContas";
 	}
